0554994a52d4bf45e232c2d37b870ad9268f5bdd
Dmytrii Demidas <forisoid@gmail.com>
2025-06-16 16:08:14 +0200

    Add files via upload

---
+++ Module_Lists_01_of_12.py
@@ -0,0 +1,12 @@
+front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
+print(front_display_list)
+
+# Your code below: 
+
+#checkpoint_2:
+print("#checkpoint_2")
+print("before: ")
+print(front_display_list)
+front_display_list.insert(0,"Pineapple")
+print("after: ")
+print(front_display_list)
